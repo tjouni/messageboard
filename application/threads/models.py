c628f92fff1e59b3264c031a293742c66179dba1
jouni tenhunen <jouni.tenhunen@gmail.com>
2019-08-22 19:04:05 +0300

    fix message order in thread view

--- application/threads/models.py
+++ application/threads/models.py
@@ -30,7 +30,8 @@ class Thread(Base):
         stmt = text("SELECT Message.id, Message.date_created, Message.date_modified,"
                     " message_text, user_id, username FROM Message"
                     " LEFT JOIN Account ON Message.user_id = Account.id"
-                    " WHERE thread_id = :tid").params(tid=t.id)
+                    " WHERE thread_id = :tid"
+                    " ORDER BY Message.id ASC").params(tid=t.id)
 
         res = db.engine.execute(stmt)
         db.session.commit()
