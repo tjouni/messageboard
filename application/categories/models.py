e2d5937f9e6b567798ccfb85608e02095a0d769c
jouni tenhunen <jouni.tenhunen@gmail.com>
2019-09-03 18:44:20 +0300

    fix sql query

--- application/categories/models.py
+++ application/categories/models.py
@@ -16,8 +16,8 @@ class Category(db.Model):
         stmt = text("SELECT Category.id, Category.name,"
                     " COUNT(Thread.id) as threadcount,"
                     " (SELECT COUNT(a.id) FROM Category AS c"
-                    "  JOIN user_category AS uc ON c.id == uc.category_id AND c.id == Category.id"
-                    "  JOIN account AS a ON uc.account_id == a.id)"
+                    "  JOIN user_category AS uc ON c.id = uc.category_id AND c.id = Category.id"
+                    "  JOIN account AS a ON uc.account_id = a.id)"
                     " AS usercount FROM Category"
                     " LEFT JOIN Thread ON Thread.category_id = Category.id"
                     " GROUP BY Category.id;")
